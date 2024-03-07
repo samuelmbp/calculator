6786f60612a14fbabb411441a509f5676389cfc9
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-07 18:01:24 +0000

    Add logic to update display when button is clicked

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
 import "./style.scss";
 
-console.log("Hello, world.");
+const calculatorDisplay = document.querySelector<HTMLInputElement>(
+    ".calculator__display"
+);
+const buttons = document.querySelectorAll<HTMLButtonElement>(
+    ".calculator__button"
+);
+
+if (!buttons || !calculatorDisplay) {
+    throw new Error("Error while pressing a button.");
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const buttonValue: string | null = button.textContent;
+
+        if (!buttonValue) return;
+
+        // Check the type of button clicked (number, operator, clear, equal) and handle the action accordingly
+        if (!isNaN(parseFloat(buttonValue)) || buttonValue === ".") {
+            // Update the display with the appropriate value or result
+            calculatorDisplay.value += buttonValue;
+        }
+    });
+});

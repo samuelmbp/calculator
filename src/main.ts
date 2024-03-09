f279b221a6e98e4518e8906c91e981a7315d395a
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-09 15:49:14 +0000

    Implement event delegation for calculator buttons to handle clicks

--- src/main.ts
+++ src/main.ts
@@ -1,90 +1,43 @@
 import "./style.scss";
 
-const calculatorDisplay = document.querySelector<HTMLInputElement>(
-    ".calculator__display"
-);
-const buttons = document.querySelectorAll<HTMLButtonElement>(
-    ".calculator__button"
-);
+// Event delegation pattern to listen to every key.
+const calculator = document.querySelector<HTMLDivElement>(".calculator");
 
-// TODO: Add these variables inside event listener (use params for the functions)
-let firstOperand: string = "";
-let secondOperand: string = "";
-let operator: string = "";
+if (!calculator) throw new Error("Error while accessing the calculator...");
 
-if (!buttons || !calculatorDisplay) {
-    throw new Error("Error while pressing a button.");
-}
+const calculatorButtons = calculator.querySelector<HTMLDivElement>(
+    ".calculator__buttons"
+    );
 
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const buttonValue: string | null = button.textContent;
+// TODO: Handle null error for buttons and display
+if (!calculatorButtons)
+    throw new Error("Error while trying to press on the buttons..");
 
-        if (!buttonValue) return;
+calculatorButtons.addEventListener("click", (event: Event) => {
+    // Returns closest ancestor of the current element or the element itself
+    const target = event.target as Element;
+    if (!target.closest("button")) return;
 
-        if (isValidNumber(buttonValue) || buttonValue === ".") {
-            calculatorDisplay.value += buttonValue;
-        } else if (buttonValue === "C") {
-            clearDisplay();
-        } else if (buttonValue === "=") {
-            calculate(firstOperand, secondOperand);
-        } else {
-            handleOperator(buttonValue);
-        }
-    });
+    const button = event.target as HTMLElement;
+    const buttonType: string | undefined = button.dataset.buttonType;
+
+    if (buttonType === "number") {
+        console.log("Pressed number");
+    }
+
+    if (buttonType === "decimal") {
+        console.log("Pressed decimal");
+    }
+
+    if (buttonType === "operator") {
+        console.log("Pressed operator");
+    }
+
+    if (buttonType === "equal") {
+        console.log("Pressed equal");
+    }
+
+    if (buttonType === "clear") {
+        console.log("Pressed clear");
+    }
 });
-
-const isValidNumber = (value: string): boolean => {
-    return !isNaN(parseFloat(value)) && isFinite(parseFloat(value));
-};
-
-const handleOperator = (operatorSymbol: string): void => {
-    if (firstOperand === "") {
-        firstOperand = calculatorDisplay.value;
-        operator = operatorSymbol;
-        calculatorDisplay.value = "";
-    } else if (secondOperand === "") {
-        // Update the operator to the most recently clicked one
-        operator = operatorSymbol;
-    }
-};
-
-const clearDisplay = (): void => {
-    calculatorDisplay.value = "";
-    firstOperand = "";
-    secondOperand = "";
-    operator = "";
-};
-
-const calculate = (firstOperand: string, secondOperand: string) => {
-    secondOperand = calculatorDisplay.value;
-    let result: number = 0;
-
-    switch (operator) {
-        case "+":
-            result = parseFloat(firstOperand) + parseFloat(secondOperand);
-            break;
-        case "-":
-            result = parseFloat(firstOperand) - parseFloat(secondOperand);
-            break;
-        case "×":
-            result = parseFloat(firstOperand) * parseFloat(secondOperand);
-            break;
-        // TODO: Show the result with only 4-6 digits?
-        case "÷":
-            result = parseFloat(firstOperand) / parseFloat(secondOperand);
-            break;
-        case "%":
-            result = parseFloat(firstOperand) % parseFloat(secondOperand);
-            break;
-        default:
-            break;
-    }
-
-    calculatorDisplay.value = result.toString();
-
-    // Reset operands and operator
-    firstOperand = "";
-    secondOperand = "";
-    operator = "";
-};

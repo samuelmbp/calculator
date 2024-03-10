9885039bda9694df3698b7e51ae756e42e3cce6a
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-10 14:56:51 +0000

    Add logic to display next number when an operator is clicked

--- src/main.ts
+++ src/main.ts
@@ -23,14 +23,31 @@ calculatorButtons.addEventListener("click", (event: Event) => {
     const button = event.target as HTMLElement;
     const buttonType: string | undefined = button.dataset.buttonType;
     const key: string | undefined = button.dataset.key;
+    const previousButtonType = calculator.dataset.previousButtonType;
     const result = calculatorDisplay.textContent;
+
+    // Release operator pressed state
+    const operatorButtons = [calculatorButtons.children].filter((button) => {
+        // Ensures it is HTMLElement
+        if (!(button instanceof HTMLElement)) return false;
+        button.dataset.buttonType === "operator";
+    });
+
+    operatorButtons.forEach((button) => {
+        if (!(button instanceof HTMLElement)) return false;
+        button.classList.remove("is-pressed");
+    });
 
     if (buttonType === "number" && key) {
         if (result === "0") {
             calculatorDisplay.textContent = key;
         } else {
-            // Append the number to the displayed result.
             calculatorDisplay.textContent = result + key;
+        }
+
+        // When previous action is an operator, show the clicked number.
+        if (previousButtonType === "operator") {
+            calculatorDisplay.textContent = key;
         }
     }
 
@@ -39,7 +56,7 @@ calculatorButtons.addEventListener("click", (event: Event) => {
     }
 
     if (buttonType === "operator") {
-        console.log("Pressed operator");
+        button.classList.add("is-pressed");
     }
 
     if (buttonType === "equal") {
@@ -49,4 +66,6 @@ calculatorButtons.addEventListener("click", (event: Event) => {
     if (buttonType === "clear") {
         console.log("Pressed clear");
     }
+
+    calculator.dataset.previousButtonType = buttonType;
 });
